1f3e8c0e6cdf37f9a5690cf1323c6c5fd9c03ff0
Haley Tibbitts <117307728+haleytibbitts@users.noreply.github.com>
2024-01-26 14:55:57 -0500

    chore: moved statement section into separate component

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -3,7 +3,6 @@ import { useRef } from "react";
 import {
   MainContainer,
   BackButton,
-  AboutParagraph,
   AboutList,
   AboutAnchorHeader,
 } from "./styled_components";
@@ -13,6 +12,7 @@ import TourInstructionsIntro from "./TourInstructionsIntro";
 import IntroNav from "./IntroNav";
 import { intro } from "../services";
 import About from "./About";
+import Statement from "./Statement";
 
 function Introduction() {
   const [, setLocation] = useLocation();
@@ -47,46 +47,7 @@ function Introduction() {
 
           <About aboutRef={aboutRef} />
 
-          <section>
-            <AboutAnchorHeader id="statement" ref={statementRef}>
-              A Statement from Walter Cassidy
-            </AboutAnchorHeader>
-            <AboutParagraph>
-              The key to this walking tour is basic. It is about local
-              visibility of the 2SLGBTQAI community. Unfortunately, much of that
-              visibility has been erased for various reasons. Some of those
-              reasons are as simple as the suppression of our identities, the
-              lack of interest in who we are, overall hate or ignorance, and our
-              own lack of seeing the importance of our stories and struggles.
-              <br />
-              <br />
-              When creating the tour, some of the examples were difficult to
-              prove if they “really” were queer, trans, or gender non-conforming
-              references, especially before the 1960s. I included them anyways
-              because one could argue either way if the experience was queer,
-              trans, or gender non-conforming.
-              <br />
-              <br />
-              I must acknowledge the hard work and dedication of the Windsor
-              Essex Rainbow Alliance (WERA). WERA is a group of individuals
-              whose goals are to institute a method of collecting, preserving,
-              and disseminating the local history of the 2SLGBTQIA+ community
-              and to establish a permanent public monument that will showcase
-              the struggles, achievements, and celebrations of the local
-              2SLGBTQIA+ community for all in Windsor/Essex and beyond to visit,
-              learn from, and enjoy. There are still so many voices, stories,
-              and places that have not been told and could be lost forever. It
-              is my goal to help change that reality.
-              <br />
-              <br />
-              If you have any materials (newsletters, pictures, posters,
-              buttons, shirts, etc.) you would like to have preserved or if you
-              find any information that is not included in this tour and would
-              like to make a request for an update or something changed, please
-              contact me{" "}
-              <a href="mailto:wequeerhistory@mdirect.net">by email</a>.
-            </AboutParagraph>
-          </section>
+          <Statement statementRef={statementRef} />
 
           <section>
             <AboutAnchorHeader id="references" ref={referencesRef}>
