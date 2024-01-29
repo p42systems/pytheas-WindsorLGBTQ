a03926ef88fe855fec851b6640462895ccb61424
Haley Tibbitts <117307728+haleytibbitts@users.noreply.github.com>
2024-01-29 11:19:58 -0500

    chore: created folder structure for content view

---
+++ src/components/Content/Content.tsx
@@ -0,0 +1,44 @@
+import { useLocation } from "wouter";
+
+import {
+  ListParagraph,
+  StaticheaderBackgroundButton,
+  BackButton,
+  ListMainContainer,
+} from "../styled_components";
+import Header from "../Header";
+import Footer from "../Footer";
+import { back } from "../../services";
+import MarkerList from "./components/MarkerList";
+
+function Content() {
+  const [, setLocation] = useLocation();
+
+  return (
+    <>
+      <Header>
+        <BackButton
+          title="Back"
+          aria-label="Back"
+          onClick={() => back(setLocation)}
+        >
+          Back
+        </BackButton>
+      </Header>
+      <ListMainContainer>
+        <ListParagraph>
+          Welcome to the content page. To view the content, click the{" "}
+          <StaticheaderBackgroundButton>View</StaticheaderBackgroundButton>
+          button. If you are near the site and would like to participate in the
+          walking tour, click the{" "}
+          <StaticheaderBackgroundButton>Take Tour</StaticheaderBackgroundButton>
+          button and it will instruct you as to where to start.
+        </ListParagraph>
+        <MarkerList />
+      </ListMainContainer>
+      <Footer />
+    </>
+  );
+}
+
+export default Content;
